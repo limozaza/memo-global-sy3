4dfca72c30d5aadc7da33790fbae371e5073f77d
limozaza <zakboite@gmail.com>
2017-12-04 20:47:00 +0100

    DoctrineEvent

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Identite;
+use AppBundle\Form\IdentiteType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,5 +18,25 @@ class DefaultController extends Controller
         $isSpam = $this->get('app.spam');
         $a = $isSpam->isSpam("Bonjour Monsieur");
         return $this->render('AppBundle:default:index.html.twig',['isSpam' => $a]);
+    }
+
+
+    /**
+     * @Route("/form", name="form")
+     */
+    public function formAction(Request $request)
+    {
+        $identite = new Identite();
+        $form = $this->createForm(IdentiteType::class, $identite);
+        $form->handleRequest($request);
+
+        if($form->isValid() && $form->isSubmitted()){
+            $em =$this->getDoctrine()->getManager();
+            $em->persist($identite);
+            $em->flush();
+            //dump($form->getData());exit();
+        }
+
+        return $this->render('AppBundle:default:form.html.twig',['form' => $form->createView()]);
     }
 }
